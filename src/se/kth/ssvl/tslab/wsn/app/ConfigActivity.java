75c173bad459d6a690412f7a81beb0059b320015
Konstantinos Vaggelakos <kozze89@gmail.com>
2012-11-24 10:14:02 +0100

    Work in progress of loading the Config UI from the config file

--- src/se/kth/ssvl/tslab/wsn/app/ConfigActivity.java
+++ src/se/kth/ssvl/tslab/wsn/app/ConfigActivity.java
@@ -2,8 +2,12 @@ package se.kth.ssvl.tslab.wsn.app;
 
 import se.kth.ssvl.tslab.wsn.R;
 import se.kth.ssvl.tslab.wsn.WSNServiceInterface;
+import se.kth.ssvl.tslab.wsn.app.config.ConfigManager;
+import se.kth.ssvl.tslab.wsn.general.servlib.config.Configuration;
 import se.kth.ssvl.tslab.wsn.service.WSNService;
 import android.app.Activity;
+import android.app.ActivityManager;
+import android.app.ActivityManager.RunningServiceInfo;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
@@ -15,24 +19,79 @@ import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 public class ConfigActivity extends Activity {
-	private CheckBox chkIos;
-	
 	private final String TAG = "ConfigActivity";
 	private WSNServiceInterface serviceInterface;
 	
+	private CheckBox checkService;
+	private EditText txtQuota;
+	private Spinner spinnerRouting;
+	private EditText serverEid;
+	private EditText serverAddress;
+ 	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_daemon_config);
-		addListenerOnChkIos();
+		getUIComponents();
+		addListenerOnChk();
 		updateUI();
 	}
 	
+	private void getUIComponents() {
+		checkService = (CheckBox) findViewById(R.id.checkService);
+		txtQuota = (EditText) findViewById(R.id.txtQuota);
+		spinnerRouting = (Spinner) findViewById(R.id.spinnerRouting);
+		serverEid = (EditText) findViewById(R.id.txtServerEid);
+		serverAddress = (EditText) findViewById(R.id.txtServerAddress);
+	}
+	
 	private void updateUI() {
+		// Read the config and show it in the UI
+		Configuration config = ConfigManager.getInstance().readConfig();
 		
+		// Set the checkbox depending on if the service is running
+		if (isServiceRunning()) {
+			checkService.setChecked(true);
+		} else {
+			checkService.setChecked(false);
+		}
+		
+		// Set the quota
+		txtQuota.setText(config.storage_setting().quota());
+		
+		// Set the routing type
+		switch (config.routes_setting().router_type()) {
+		case STATIC_BUNDLE_ROUTER:
+			spinnerRouting.setSelection(0);
+			break;
+		case EPIDEMIC_BUNDLE_ROUTER:
+			spinnerRouting.setSelection(1);
+			break;
+		case PROPHET_BUNDLE_ROUTER:
+			spinnerRouting.setSelection(2);
+			break;
+		default:
+			Log.e(TAG, "Don't know what to specify in the routing spinner. Is the router type undefined?");
+			break;
+		}
+		
+		// Set the server eid from shared prefs
+		
+	}
+	
+	private boolean isServiceRunning() {
+	    ActivityManager manager = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
+	    for (RunningServiceInfo service : manager.getRunningServices(Integer.MAX_VALUE)) {
+	        if (WSNService.class.getName().equals(service.service.getClassName())) {
+	            return true;
+	        }
+	    }
+	    return false;
 	}
 	
 	/**
@@ -51,11 +110,8 @@ public class ConfigActivity extends Activity {
 		
 	}
 	
-
-	public void addListenerOnChkIos() {
-		chkIos = (CheckBox) findViewById(R.id.checkBox1);
-
-		chkIos.setOnClickListener(new OnClickListener() {
+	public void addListenerOnChk() {
+		checkService.setOnClickListener(new OnClickListener() {
 
 			@Override
 			public void onClick(View v) {
